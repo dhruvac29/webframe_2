1cbf4fa1c0a9ac2ca9ac1b3a71a8fbc1217ec1d6
dhruvac29 <dhruvac29@gmail.com>
2024-06-23 14:58:38 -0500

    V1.0

--- webframe_2/src/ChemicalEcotoxicity.jsx
+++ webframe_2/src/ChemicalEcotoxicity.jsx
@@ -1,61 +1,137 @@
-import React, { useState, useEffect } from "react"; // Import useState and useEffect
-import {
-  Skeleton,
-  Card,
-  message,
-  Upload,
-  Button,
-  Row,
-  Col,
-  Statistic,
-} from "antd";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import {
-  UploadOutlined,
-  ArrowDownOutlined,
-  ArrowUpOutlined,
-} from "@ant-design/icons";
+import React, { useState, useEffect } from "react";
+import { Skeleton, Card, message, Upload, Button } from "antd";
+import { UploadOutlined, ArrowDownOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 const ChemicalEcotoxicity = () => {
   const [loading, setLoading] = useState(true);
   const [isRunClicked, setIsRunClicked] = useState(false);
-  const [hc50Value, setHc50Value] = useState(null);
+  const [csvUrl, setCsvUrl] = useState("");
+
+  const [fileList, setFileList] = useState([]);
+  const [binaryData, setBinaryData] = useState(null);
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 2000);
     return () => clearTimeout(timer);
   }, []);
 
-  const handleRunClick = () => {
-    setIsRunClicked(true);
+  const handleRunClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://f4etbkx7i0.execute-api.us-east-1.amazonaws.com/uploads"
+      );
+
+      if (response.status === 200) {
+        const { uploadURL, Key } = response.data;
+
+        const response2 = await axios.put(uploadURL, binaryData, {
+          headers: {
+            "Content-Type": "text/csv",
+          },
+        });
+        if (response2.status === 200) {
+          try {
+            const response = await axios.post(
+              "https://inewqvb5i7.execute-api.us-east-1.amazonaws.com/default/HC50",
+              { fileName: Key }
+            );
+
+            if (response.status === 200) {
+              console.log(response.data);
+            }
+          } catch (error) {
+            console.error("Error calling Lambda function: ", error);
+          }
+        }
+      } else {
+        // onError(new Error("Failed to get pre-signed URL."));
+        message.error("Failed to get pre-signed URL.");
+      }
+    } catch (err) {
+      console.error("Error uploading file: ", err);
+      message.error("Error uploading file.");
+    }
   };
 
   const handleFileChange = async (info) => {
-    if (info.file.status === "uploading") {
-      return;
+    console.log(info);
+  };
+
+  const onRemove = (file) => {
+    const index = fileList.indexOf(file);
+    const newFileList = fileList.slice();
+    newFileList.splice(index, 1);
+    setFileList(newFileList);
+  };
+
+  const beforeUpload = (file) => {
+    setFileList([...fileList, file]);
+    if (file) {
+      readFile(file);
     }
-    if (info.file.status === "done") {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
+
+    return false;
+  };
+
+  const readFile = (file) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const csvContent = event.target.result;
+      convertToBinary(csvContent);
+    };
+    reader.readAsText(file);
+  };
+
+  const convertToBinary = (csvContent) => {
+    const binary = new Blob([csvContent], { type: "text/csv" });
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const arrayBuffer = reader.result;
+      setBinaryData(arrayBuffer);
+    };
+    reader.readAsArrayBuffer(binary);
   };
 
   const customRequest = async ({ file, onSuccess, onError }) => {
     try {
-      const formData = new FormData();
-      formData.append("file", file);
+      console.log(file.file);
+      // Get a pre-signed URL from your backend
+      const response = await axios.get(
+        "https://f4etbkx7i0.execute-api.us-east-1.amazonaws.com/uploads"
+      );
 
-      await axios.post("http://localhost:3000/api/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      if (response.status === 200) {
+        const { uploadURL, Key } = response.data;
 
-      onSuccess("OK");
+        // Create FormData to upload the file to S3
+        let binary = atob(this.csv.split(",")[1]);
+        let array = [];
+        for (var i = 0; i < binary.length; i++) {
+          array.push(binary.charCodeAt(i));
+        }
+        let blobData = new Blob([new Uint8Array(array)], { type: "text/csv" });
+
+        console.log("Uploading to: ", uploadURL);
+
+        const result = await fetch(uploadURL, {
+          method: "PUT",
+          body: blobData,
+        });
+
+        if (result.ok) {
+          console.log("File uploaded successfully.");
+        } else {
+          console.error("Error uploading file:", result.statusText);
+        }
+      } else {
+        onError(new Error("Failed to get pre-signed URL."));
+        message.error("Failed to get pre-signed URL.");
+      }
     } catch (err) {
       console.error("Error uploading file: ", err);
       onError(err);
+      message.error("Error uploading file.");
     }
   };
 
@@ -69,7 +145,7 @@ const ChemicalEcotoxicity = () => {
     justifyContent: "center",
     backgroundColor: "#f0f2f5",
     border: "1px solid #d9d9d9",
-    cursor: "pointer", // Add cursor style to indicate it's clickable
+    cursor: "pointer",
   };
 
   const centeredStyle = {
@@ -78,18 +154,17 @@ const ChemicalEcotoxicity = () => {
     marginTop: "20px",
   };
 
-  const statisticCardStyle = {
-    width: 200, // Set the width of the Statistic card here
-    margin: "0 auto",
-  };
-
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
       <Skeleton loading={loading} active>
         <Card style={cardStyle}>Chemical Ecotoxicity</Card>
       </Skeleton>
-      <Upload customRequest={customRequest} onChange={handleFileChange}>
-        <Button>Click to Upload</Button>
+      <Upload
+        onRemove={onRemove}
+        beforeUpload={beforeUpload}
+        fileList={fileList}
+      >
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
       </Upload>
       <Button
         type="primary"
@@ -99,24 +174,13 @@ const ChemicalEcotoxicity = () => {
         Run
       </Button>
       {isRunClicked && (
-        <>
-          <h2 style={{ marginTop: "120px" }}>Model Prediction</h2>
-          <Row gutter={16} style={centeredStyle}>
-            <Col>
-              <Card bordered={false} style={statisticCardStyle}>
-                <Statistic
-                  title="HC50 Value"
-                  value={hc50Value}
-                  precision={2}
-                  valueStyle={{
-                    color: "#3f8600",
-                  }}
-                  //   prefix={<ArrowUpOutlined />}
-                />
-              </Card>
-            </Col>
-          </Row>
-        </>
+        <div style={centeredStyle}>
+          <a href={csvUrl} download="hc50_value.csv">
+            <Button icon={<ArrowDownOutlined />} style={{ marginTop: "20px" }}>
+              Download CSV
+            </Button>
+          </a>
+        </div>
       )}
     </div>
   );
